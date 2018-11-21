c864e0387e4734c6944fddcf0e995d4da35ec5af
Marcelo Regueira <mregueira@itba.edu.ar>
2018-11-21 02:04:46 -0300

    entregado

--- Graficos/BodeSuperpuesto.m
+++ Graficos/BodeSuperpuesto.m
@@ -9,13 +9,13 @@ for i=1:3001
 end
 pic = semilogx(freq_Sim,ratio_Sim,'color','blue','linewidth',2);
 xlim([1000 1000000]);
-ylim([25 30]);
-
+ylim([20 30]);
+grid on;
 title('Respuesta en frecuencia - Módulo');
 legend('Simulado');
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 12);
 xlabel('Frecuencia [Hz]');         
 ylabel('|AV| [dB]');
-
+grid on;
 set(gcf,'PaperOrientation','landscape');
 print('BodeModuloSimple','-dpdf','-fillpage');
--- Graficos/DrainComun.m
+++ Graficos/DrainComun.m
@@ -1,14 +1,14 @@
-% Medicion diferencial %
+% Medicion Drain Comun %
 
 pic = plot(time,vin,'color','blue','linewidth',2);
 hold on;
 pic = plot(time,vout,'color','red','linewidth',2);
-
-title('Medición par diferencial');
+grid on;
+title('Medición Drain Comun');
 legend('CH1 - Entrada','CH2 - Salida');
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 12);
 xlabel('Tiempo [s]');         
 ylabel('Tension [V]');
-grid on;
+
 set(gcf,'PaperOrientation','landscape');
-print('OutDiferencial','-dpdf','-fillpage');
+print('OutDrainComun','-dpdf','-fillpage');
